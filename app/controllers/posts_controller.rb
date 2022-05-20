95c35743fc80def58eededdc3c1cbccaa9fdcb40
tomgazzard1 <102549458+tomgazzard1@users.noreply.github.com>
2022-05-20 09:26:50 -0500

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+    def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new
+      @post["body"] = params["post"]["body"]
+      @post["image"] = params["post"]["image"]
+      @post.save
+      redirect_to "/posts"
+    end
+  
+  end
