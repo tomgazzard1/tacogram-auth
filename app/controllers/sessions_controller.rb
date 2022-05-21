e381f5c3945d01b9f5969cc3e33525848e027491
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-05-21 00:20:31 +0000

    Authenticating User start

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,10 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
-  end
+    @user = User.find_by({"email"} => params["email"]) # Check to see if submitted email is in database. Not nested since not /sessions not DB backed 
+    if @user # Checks to see if the user exists in the DB based on the email submitted
+      redirect_to "/posts"
+    else
+      redirect_to "/sessions/new"
+    end
 end
